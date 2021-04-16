07eb8f47554dcce9c4268aae728bc63916d6252a
Norman Bernard <nksb414@gmail.com>
2021-04-16 17:30:55 +0800

    completed the baby book page

---
+++ assets/js/scriptbabybook.js
@@ -0,0 +1,15 @@
+//var babyBookLink = document.querySelector('#babybooklink');
+console.log("hello");
+debugger
+var userDetails = JSON.parse(localStorage.getItem("SavedProfileDetails")) || [{
+    userName: "",
+    babyName: "",
+    babyWeight: "",
+    babyDob: "",
+    lastBreast: "",
+}]
+
+
+document.getElementById("babynameptag").textContent=userDetails[0].babyName;
+document.getElementById("babyweightptag").textContent=userDetails[0].babyWeight;
+document.getElementById("babydobptag").textContent=userDetails[0].babyDob;
