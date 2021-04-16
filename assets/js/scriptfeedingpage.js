eea1da6cf3b9e138b73d0d09b8b8e74276b2a3ec
Brett Treweek <bretttrew@gmail.com>
2021-04-16 13:34:02 +0800

    added timer,swap/stop buttons

--- assets/js/scriptfeedingpage.js
+++ assets/js/scriptfeedingpage.js
@@ -2,6 +2,9 @@ var leftBtn = document.querySelector('#feedstartleft')
 var rightBtn = document.querySelector('#feedstartright')
 var lastSideH1 = document.querySelector('#last-side')
 var sideSpan = document.querySelector('#side')
+var startBtnDiv = document.querySelector('#div-with-start-feed-btns')
+var timerDiv = document.querySelector('#timer-div')
+var timer = document.querySelector('#timer')
 
 var userDetails = JSON.parse(localStorage.getItem("SavedProfileDetails")) || [{
     userName: "",
@@ -26,13 +29,24 @@ rightBtn.addEventListener('click', feedRight)
 
 function feedLeft(){
     userDetails[0].lastBreast = 'Left'
-    
+    startBtnDiv.setAttribute('style', 'display:none')
+    startTimer()
 }
 function feedRight(){
     userDetails[0].lastBreast = 'Right'
+    startBtnDiv.setAttribute('style', 'display:none')
+    startTimer()
 }
 
-
+// Sets userDetails with updated last side to local storage.
+// Starts timer/ records time of feed
+// Display Quote of the day?
+// Display stop/swap buttons?
+// Record time and duration of feed?
+function startTimer(){
+    localStorage.setItem("SavedProfileDetails",JSON.stringify(userDetails))
+    timerDiv.setAttribute('style', 'display:block')
+}
 
 
 
